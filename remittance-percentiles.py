45a1c078cfe87a75573b9d596cd4ef2bc1082698
Nadia Bey <45720698+nadiabey@users.noreply.github.com>
2025-11-06 20:20:22 -0500

    Create remittance-percentiles.py

---
+++ remittance-percentiles.py
@@ -0,0 +1,55 @@
+"""
+the 2026 proposed hospital price transparency rule requires hospitals to disclose the 10th percentile, median, and 90th percentile price for all offered services in standard charge machine-readable files using remittance data. if the calculated median/percentile is not a real price, the value must be replaced with the closest real price. this uses remittance tables from epic to estimate these benchmarks for each unique billing code.
+"""
+import pandas as pd, sqlite3, re, numpy as np
+
+
+# function to get nearest value
+def nearest(listy, val):
+    return listy[np.absolute(listy-val).argmin()]
+
+
+# open db
+conn = sqlite3.connect('dbname.db')
+# get remittance image id, code, line item amount, and amount paid toward claim from one table
+# get invoice number, claim code and patient responsibility from other table
+# some remittances have charges removed or insurance denied so drop those
+remit = pd.read_sql("select rmt.image_id, inv_no, proc_identifier, prov_payment_amt "
+                    "from cl_rmt_svce_ln_inf as rmt join cl_rmt_clm_info as clm on rmt.image_id = "
+                    "clm.image_id where inv_no not in (select inv_no from cl_rmt_clm_info where clm_stat_cd_c_name = "
+                    "'Reversal of previous payment' or clm_stat_cd_c_name = 'Denied')", conn)
+conn.close()
+# payment columns are strings so change to floats
+remit['PROV_PAYMENT_AMT'] = remit['PROV_PAYMENT_AMT'].astype(float)
+# transform code column into three columns (code type, code, modifier)
+codes = [re.split(r'[:>]+', code) for code in remit['PROC_IDENTIFIER']]
+code_type = []
+code = []
+modifier = []
+for cd in codes:
+    code_type.append(cd[0])
+    code.append(cd[1])
+    mod = "" if len(cd) < 3 else cd[2]  # empty column if no modifier else modifier
+    modifier.append(mod)
+# set new columns
+remit['code_type'] = code_type
+remit['code'] = code
+remit['modifier'] = modifier
+# simplify columns
+new_cols = remit[['code_type', 'code', 'modifier', 'PROV_PAYMENT_AMT']]
+# calculate percentiles and claim counts
+stats = new_cols.groupby(['code_type', 'code', 'modifier'], as_index=False).describe([.1, .5, .9])
+# group prices
+prices = new_cols.groupby(['code_type', 'code', 'modifier'], as_index=False)['PROV_PAYMENT_AMT'].apply(list)
+# check if 10th/median/90th percentile values are real prices
+for i in range(len(stats)):
+    payments = prices.iloc[i]['PROV_PAYMENT_AMT'] # list of prices
+    # store current values and if current values don't match list, calculate new value
+    ten = stats.iloc[i]['PROV_PAYMENT_AMT']['10%']
+    perc10 = ten if ten in payments else nearest(payments, ten)
+    median = stats.iloc[i]['PROV_PAYMENT_AMT']['50%']
+    perc50 = median if median in payments else nearest(payments, median)
+    ninety = stats.iloc[i]['PROV_PAYMENT_AMT']['90%']
+    perc90 = ninety if ninety in payments else nearest(payments, ninety)
+    # replace values in table
+    stats.loc[i, ('PROV_PAYMENT_AMT', '10%')], stats.loc[i, ('PROV_PAYMENT_AMT', '50%')], stats.loc[i, ('PROV_PAYMENT_AMT', '90%')] = perc10, perc50, perc90
